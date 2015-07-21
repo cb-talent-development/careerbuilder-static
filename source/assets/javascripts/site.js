8754c5476f63ce79f1e9ebf9b0c75f35f4011a0b
jonathan-eyler-werve <jonathan@eylerwerve.com>
2015-07-21 17:05:54 -0500

    adds Interactions of flyouts in JS

--- source/assets/javascripts/site.js
+++ source/assets/javascripts/site.js
@@ -1,22 +1,45 @@
 //= require_tree .
 //= require_self
 
+window.interact = {}
+
+window.interact.watchApplyDiscount = function() {
+
+  panel = $('#apply-discount')
+  links = $('.cart__apply-discount-link')
+  panel.hidden = true;
+  panel.hide();
+  links.click( function() {
+    panel.hidden = !panel.hidden
+    if (panel.hidden == true) {
+      panel.hide()
+    }
+    else {
+      panel.slideDown(150)
+    }
+  });
+};
+
+window.interact.watchMobileMenu = function() {
+
+  mobileMenuLink = $('#mobile-menu-link');
+  mobileMenu = $('#mobile-menu');
+  mobileMenu.hidden = true;
+  mobileMenu.hide();
+  mobileMenuLink.click(function() { 
+    mobileMenu.hidden = !mobileMenu.hidden
+    if (mobileMenu.hidden == true) {
+      mobileMenu.hide();
+    }
+    else {
+      mobileMenu.slideDown(150)
+    };
+  });
+}; 
+
 jQuery(function(){
   console.log('site.js working');
 
-  mobileMenuLink = document.getElementById('mobile-menu-link');
-  mobileMenu = document.getElementById('mobile-menu');
-  mobileMenu.hidden = true
-
-  mobileMenuLink.onclick = function() { 
-    mobileMenu.hidden = !mobileMenu.hidden
-
-    if (mobileMenu.hidden == true) {
-      $(mobileMenu).addClass('hidden')
-    }
-    else {
-      $(mobileMenu).removeClass('hidden')
-    }
-  }
-
+  window.interact.watchMobileMenu();
+  window.interact.watchApplyDiscount();
 })
