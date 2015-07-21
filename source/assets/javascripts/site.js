8197933cd6357e3be7385794bce4debcc147842c
jonathan-eyler-werve <jonathan@eylerwerve.com>
2015-07-21 15:17:41 -0500

    Mobile menu JS working, add jQuery link

--- source/assets/javascripts/site.js
+++ source/assets/javascripts/site.js
@@ -1,6 +1,22 @@
 //= require_tree .
 //= require_self
 
-(function(){
-  console.log('working');
-})(); 
+jQuery(function(){
+  console.log('site.js working');
+
+  mobileMenuLink = document.getElementById('mobile-menu-link');
+  mobileMenu = document.getElementById('mobile-menu');
+  mobileMenu.hidden = true
+
+  mobileMenuLink.onclick = function() { 
+    mobileMenu.hidden = !mobileMenu.hidden
+
+    if (mobileMenu.hidden == true) {
+      $(mobileMenu).addClass('hidden')
+    }
+    else {
+      $(mobileMenu).removeClass('hidden')
+    }
+  }
+
+})
