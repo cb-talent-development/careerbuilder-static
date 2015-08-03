af48dd8b402e9513f86e6e3fcf8262a8fb694074
jonathan-eyler-werve <jonathan@eylerwerve.com>
2015-08-03 16:17:18 -0500

    modals are working

--- source/assets/javascripts/site.js
+++ source/assets/javascripts/site.js
@@ -44,9 +44,47 @@ window.interact.watchMobileMenu = function() {
   });
 }; 
 
+window.interact.modal = function(modalName, openLink) {
+  
+  openLinks = $('.' + openLink)
+  closeLinks = $('.close-modal');
+  modal = {}
+  modal.back = $('#modal-background')
+  modal.form = $('#' + modalName), 
+  modal.hidden = true;
+  modal.form.hide();
+  modal.back.hide();
+  modal.form.removeClass('hidden');
+  modal.back.removeClass('hidden');
+  
+  closeLinks.click(function() { 
+    // console.log("modal close: " + modalName)
+    modal.hidden = true
+    modal.form.hide();
+    modal.back.hide();
+  });
+
+  openLinks.click(function() { 
+    // console.log("modal open: " + modalName)
+    modal.hidden = false
+
+    // guards against showing background when modal not found 
+    if (modal.form.length >= 1) {
+      modal.back.show();
+      modal.form.show(); 
+      }
+    else {
+      console.log("ERROR: you asked for a modal named #" + modalName + " but it wasn't found on the page.")  
+    };
+  });
+}; 
+
 jQuery(function(){
   console.log('site.js working');
 
+  // modals must be added manually, this is a likely refactor if this gets hard
+  window.interact.modal('modal--change-billing-address', 'modal-open--change-billing-address')
+  window.interact.modal('modal--change-company-address', 'modal-open--change-company-address')
   window.interact.watchMobileMenu();
   window.interact.watchApplyDiscount();
 })
