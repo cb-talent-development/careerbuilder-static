c191c0b1b2eaf935204c2a11e8a12e88d9dd727f
jonathan-eyler-werve <jonathan@eylerwerve.com>
2015-08-04 09:35:00 -0500

    Bug fix, resolves JS scope error
    Apparently local scope bleeds over between calls on a function?
    Something in jQuery? Mysterious.

--- source/assets/javascripts/site.js
+++ source/assets/javascripts/site.js
@@ -46,35 +46,34 @@ window.interact.watchMobileMenu = function() {
 
 window.interact.modal = function(modalName, openLink) {
   
-  openLinks = $('.' + openLink)
+  openLinks = $('.' + openLink);
   closeLinks = $('.close-modal');
-  modal = {}
-  modal.back = $('#modal-background')
-  modal.form = $('#' + modalName), 
-  modal.hidden = true;
-  modal.form.hide();
-  modal.back.hide();
-  modal.form.removeClass('hidden');
-  modal.back.removeClass('hidden');
+  modalBack = $('#modal-background');
+  modalForm = $('#' + modalName); 
+  // modals start with '.hidden' in HTML to prevent load order race between HTML/JS
+  modalForm.hide();
+  modalBack.hide();
+  modalForm.removeClass('hidden');
+  modalBack.removeClass('hidden');
   
   closeLinks.click(function() { 
-    // console.log("modal close: " + modalName)
-    modal.hidden = true
-    modal.form.hide();
-    modal.back.hide();
+    // binding to all close links; click on one modal hides all modals 
+    console.log("modal close: " + modalName);
+    modalForm.hide();
+    modalBack.hide();
   });
 
   openLinks.click(function() { 
-    // console.log("modal open: " + modalName)
-    modal.hidden = false
-
+    console.log("modal open: " + modalName);
+    // required
+    modalForm = $('#' + modalName); 
     // guards against showing background when modal not found 
-    if (modal.form.length >= 1) {
-      modal.back.show();
-      modal.form.show(); 
+    if (modalForm.length >= 1) {
+      modalBack.show();
+      modalForm.show(); 
       }
     else {
-      console.log("ERROR: you asked for a modal named #" + modalName + " but it wasn't found on the page.")  
+      console.log("ERROR: link asked for a modal named #" + modalName + " but it wasn't found on the page.")  
     };
   });
 }; 
@@ -83,6 +82,7 @@ jQuery(function(){
   console.log('site.js working');
 
   // modals must be added manually, this is a likely refactor if this gets hard
+  // arguements are ( 'my-modal-id' , 'class-on-links-to-open' ) just like that.
   window.interact.modal('modal--change-billing-address', 'modal-open--change-billing-address')
   window.interact.modal('modal--change-company-address', 'modal-open--change-company-address')
   window.interact.watchMobileMenu();
