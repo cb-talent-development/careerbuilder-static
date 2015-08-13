511d0a1853202937e14c379b13402f7574ac3e1e
jonathan-eyler-werve <jonathan@eylerwerve.com>
2015-08-13 17:07:22 -0500

    YouTube embed modals for homepage

--- source/assets/javascripts/interactions/modal.js
+++ source/assets/javascripts/interactions/modal.js
@@ -7,6 +7,7 @@ window.interact.modal = function(modalName, openLink) {
   var closeLinks = $('.close-modal');
   var modalBack  = $('#modal-background');
   var modalForm  = $('#' + modalName);
+  var video, videoSource;
 
   // modals start with '.hidden' in HTML to prevent load order race between HTML/JS
   modalForm.hide();
@@ -16,17 +17,39 @@ window.interact.modal = function(modalName, openLink) {
 
   // binding to all close links; click on one modal hides all modals
   closeLinks.click(function() {
+    event.preventDefault();
+
+    video = $('#' + modalName + " iframe")
+
+    if (video.length > 0) {
+      // nukes (then restores) video source to stop playback
+      // actual pausePlayback API requires cross origin Window message, possibly blocked
+      videoSource = $(video).attr("src");
+      $(video).attr("src","");
+      $(video).attr("src",videoSource);
+    };
+
     modalForm.hide();
     modalBack.hide();
+
   });
 
   openLinks.click(function() {
+    event.preventDefault();
+
     // required
     modalForm = $('#' + modalName);
+    video = $('#' + modalName + " iframe")
+
     // guards against showing background when modal not found
     if (modalForm.length >= 1) {
-      // modals can scroll, but located at the top of the page
-      window.scrollTo(0, 0);
+      
+      // default modals can scroll, but located at the top of the page
+      // .modal--video does not scroll, position fixed
+      if (!modalForm.hasClass("modal--video")) {
+        window.scrollTo(0, 0);
+      };
+
       modalBack.show();
       modalForm.show();
     }
