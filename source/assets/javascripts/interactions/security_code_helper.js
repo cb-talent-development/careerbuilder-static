743eb1a586e8e9bf0936fc81394fc8ae76b1b049
jonathan-eyler-werve <jonathan@eylerwerve.com>
2015-08-14 11:58:03 -0500

    bug fix credit card helper JS

--- source/assets/javascripts/interactions/security_code_helper.js
+++ source/assets/javascripts/interactions/security_code_helper.js
@@ -7,6 +7,7 @@ window.interact.securityCodeHelper = function (target, payload) {
   var mastercard = /^5[1-5][0-9]{5,}/;
   var amex       = /^3[47][0-9]{5,}/;
   var discover   = /^6(?:011|5[0-9]{2})[0-9]{3,}/;
+  var input;
 
   $(payload).hide();
   $(payload).removeClass("hidden");
